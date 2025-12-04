17b616d2f87b40a45d45fe37d6210658508951ba
Tim <tim.mcallister84@gmail.com>
2025-12-04 15:20:24 -0600

    Update batteries.py

--- 3/batteries.py
+++ 3/batteries.py
@@ -4,11 +4,15 @@ def battery():
         lines = f.readlines()
         for line in lines:
             maxes = []
-            indexes = [0]
-            for i in range(13, 1, -1):
-                ordered  = sorted(line.strip()[indexes[-1]+1:-i], reverse=True)
+            indexes = [-1]
+            for i in range(11, -1, -1):
+                if i > 0:
+                    ordered  = sorted(line.strip()[indexes[-1]+1:-i], reverse=True)
+                else:
+                    ordered = sorted(line.strip()[indexes[-1] + 1:], reverse=True)
                 maxes.append(ordered[0])
-                indexes.append(line.index(maxes[-1]))
+                indexes.append(line.index(maxes[-1], indexes[-1] + 1))
+            sum += int(''.join(maxes))
 
     return sum
 print(battery())
